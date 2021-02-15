f77d38da4303126e271f15ea6611f5adebd5babc
controlvee <adam.goerlich@gmail.com>
2021-02-14 18:41:32 -0600

    stable 1.0

--- ControlVee.Port.InventoryManagement.DutShop.Test/Models/InventoryOnHandModel.cs
+++ ControlVee.Port.InventoryManagement.DutShop.Test/Models/InventoryOnHandModel.cs
@@ -4,13 +4,14 @@ using System.Text;
 
 namespace ControlVee.Port.InventoryManagement.DutShop.Test.Models
 {
+    [Serializable()]
     public class InventoryOnHandModel
     {
         public int ID { get; set; }
         public string NameOf { get; set; }
         public int Total { get; set; }
-        public DateTime Completion { get; set; }
-        public DateTime Expiration { get; set; }
+        public DateTime? Completion { get; set; }
+        public DateTime? Expiration { get; set; }
         public int BatchId { get; set; }
 
         public InventoryOnHandModel()
